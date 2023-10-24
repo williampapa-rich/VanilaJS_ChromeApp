9cf2ceb37afa46178a8be85d2707c78ebb1a873f
William <william@Williamui-iMac.local>
2023-10-25 00:18:53 +0900

    2.10강까지 들음

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-function plus(a, b) {
-  console.log(typeof a);
-  console.log(typeof b);
-}
+const cal = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  subject: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-plus(a, b);
+cal.add(1, 2);
+cal.minus(3, 1);
+cal.subject(2, 3);
+cal.power(2, 3);
