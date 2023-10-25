9f6f26caf9d388da23a0e2eb2d4f19d9a70e75c6
William <william@Williamui-iMac.local>
2023-10-26 08:47:50 +0900

    3.2강까지 들음

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const age = parseInt(prompt("당신의 나이는?"));
+const title = document.querySelectorAll(".hello h1");
 
-if (isNaN(age)) {
-  console.log("숫자만 입력 가능");
-} else if (age < 18) {
-  console.log("미성년자 ㅈㅅ");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age == 100) {
-  console.log("You are 100");
-} else {
-  console.log("노인 ㅈㅅ");
-}
+console.log(title);
