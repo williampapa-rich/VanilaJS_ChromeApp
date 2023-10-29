4df2ce699bff175f8437d1d69c0abf97d016afe7
William <william@Williamui-iMac.local>
2023-10-29 21:40:20 +0900

    3.5강까지 들었음

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-const title = document.querySelectorAll(".hello h1");
+const h1 = document.querySelector(".hello");
 
-console.log(title);
+console.dir(h1);
+
+function handleh1Click() {
+  h1.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  h1.innerText = "Mouse is Here!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse has left";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("Copy Cat!");
+}
+
+function handleWindowOffline() {
+  alert("SOS No WiFi");
+}
+
+function handleWindowOnline() {
+  alert("Okay connected!");
+}
+
+h1.addEventListener("click", handleh1Click);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("onfline", handleWindowOnline);
