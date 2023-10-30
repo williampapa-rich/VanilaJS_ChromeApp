0ea40b1da3d2d0f26fa86b942e3eadf8d361b9a3
makwang-woon <mku6741@gmail.com>
2023-10-30 12:55:02 +0900

    3.8강까지 들음

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const h1 = document.querySelector(".hello");
 
 function handleh1Click() {
-  const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  } else {
-    h1.className = clickedClass;
-  }
+  h1.classList.toggle("clicked");
 }
+
 
 h1.addEventListener("click", handleh1Click);
