6a4e33bc315eb44924098b517985bdfb800ac495
makwang-woon <mku6741@gmail.com>
2023-11-01 01:30:34 +0900

    4.4강까지 들음

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.classList.toggle(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello, ${username}`;
 }
 
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
