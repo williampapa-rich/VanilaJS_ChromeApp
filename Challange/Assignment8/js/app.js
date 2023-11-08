1533dd78d6343d9e04dfaf112388e62f434a97f0
William <william@Williamui-iMac.local>
2023-11-08 21:52:56 +0900

    Assignment 8 제출 완료

--- Challange/Assignment8/js/app.js
+++ Challange/Assignment8/js/app.js
@@ -1,22 +1,26 @@
-const dDay = new Date('2023-12-25 00:00:00');
-const currentTime = new Date();
+const countDownPanel = document.getElementById("countDown-panel");
 
-const timeLeft = dDay - currentTime;
+function countDownMachine() {
+  const dDay = new Date("2023-12-25 00:00:00");
+  const now = new Date();
+  const timeLeft = dDay - now;
 
-const year = currentTime.getFullYear();
-const month = currentTime.getMonth();
-const day = currentTime.getDay();
-const hour = currentTime.getHours();
-const minute = currentTime.getMinutes();
-const second = currentTime.getSeconds();
+  const day = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
+  const hour = Math.floor(timeLeft / (60 * 60 * 1000)) - day * 24;
+  const minute = Math.floor(timeLeft / (60 * 1000)) - day * 24 * 60 - hour * 60;
+  const second =
+    Math.floor(timeLeft / 1000) -
+    day * 24 * 60 * 60 -
+    hour * 60 * 60 -
+    minute * 60;
 
-const testTime1 = new Date('2023-12-25 00:00:00');
-const testTime2 = new Date('2023-12-26 00:00:00');
+  const countDown = `${day}d ${hour}h ${minute}m ${second}s`;
+  countDownPanel.innerText = countDown;
+}
 
-const now = Date.now();
+countDownMachine();
+setInterval(countDownMachine, 1000);
 
-console.log(testTime2-testTime1);
-
-
-
-
+// const hour =
+// const minute =
+// const second =
